7f35c5b1cc383ea9a41b28fac1fd6ffb7070ee66
luis lopez <luislopez1218@yahoo.com>
2019-10-30 18:01:44 -0700

    done

---
+++ Currency Converter/Currency Converter/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency_Converter
+{
+    public partial class currencyConverter : Form
+    {
+        public currencyConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebBrowser.Navigate("https://www.foreignexchangeresource.com/currency-converter.php?c=CAD&a=USD&amt=1&panel=1&button=2");
+        }
+    }
+}
